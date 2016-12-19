542f3267b306673aa89f7b33553a4d84bc5e580f
Kai Xie <ovlesser@live.com>
2016-12-19 14:42:43 +1100

    database added

---
+++ app/src/main/java/ovlesser/coolweather/model/City.java
@@ -0,0 +1,50 @@
+package ovlesser.coolweather.model;
+
+/**
+ * Created by ovlesser on 19/12/2016.
+ */
+
+public class City {
+
+    private int id;
+
+    private String cityName;
+
+    private String cityCode;
+
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+}
